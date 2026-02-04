7da1bdde7d0202aec086415bdf335a9f66ba0ac7
senkei. <cescr912@gmail.com>
2026-02-04 16:59:55 +0900

    Create UserPlaylist.js

---
+++ UserPlaylist.js
@@ -0,0 +1,32 @@
+[
+  {
+    "track": "That Way",
+    "artist": "Lil Uzi Vert",
+    "genre": "hiphop",
+    "danceability": 0.73,
+    "loudness": -6.6,
+    "valence": 0.50,
+    "duration_s": 212,
+    "energy": 0.68
+  },
+  {
+    "track": "HUMBLE.",
+    "artist": "Kendrick Lamar",
+    "genre": "hiphop",
+    "danceability": 0.79,
+    "loudness": -5.2,
+    "valence": 0.42,
+    "duration_s": 177,
+    "energy": 0.86
+  },
+  {
+    "track": "Merry Christmas, Mr. Lawrence",
+    "artist": "坂本龍一",
+    "genre": "Contemporary Classical",
+    "danceability": 0.28,
+    "loudness": -17.5,
+    "valence": 0.62,
+    "duration_s": 245,
+    "energy": 0.34
+  }
+]
